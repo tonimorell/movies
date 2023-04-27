769365e1158059baba5acea4fd65b9715e1507b8
Antonio Morell Bennasser <morellbennasser@gmail.com>
2023-04-27 09:08:31 +0200

    feat: add styles to the cards

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -5,8 +5,14 @@
 <div class="card">
 	<h2>{movie.title}</h2>
 	<img class="poster" src={movie.posterURLs[780]} alt={movie.title} />
-	<p><span>Description: </span>{movie.overview}</p>
-	<p><span>Directors: </span>{movie.directors.join(', ')}</p>
+	<div class="description">
+		<span>Description: </span>
+		<p>{movie.overview}</p>
+	</div>
+	<div>
+		<span>Directors: </span>
+		<p>{movie.directors.join(', ')}</p>
+	</div>
 </div>
 
 <style>
@@ -17,6 +23,7 @@
 		justify-content: flex-start;
 		padding: 1em;
 		max-width: 30em;
+		max-height: 50em;
 		margin: 0 auto;
 		border: 1px solid #ccc;
 		border-radius: 4px;
@@ -25,7 +32,25 @@
 		max-width: 100%;
 		max-height: 100%;
 	}
+	.description {
+		margin-bottom: 1rem;
+		width: 100%;
+		height: 150px;
+		overflow: hidden;
+	}
+	.description p {
+		height: 120px;
+		text-overflow: ellipsis;
+	}
 	span {
 		font-weight: bold;
+	}
+	h2 {
+		margin-bottom: 2rem;
+		font-weight: bold;
+		font-size: 1.5rem;
+	}
+	img {
+		margin-bottom: 1rem;
 	}
 </style>
